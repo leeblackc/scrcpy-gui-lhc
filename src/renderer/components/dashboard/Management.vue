e78ee76e117fffd004f1da95e7e8ccdc76ce7bba
lhc0821 <1013217387@qq.com>
2019-12-06 15:46:28 +0800

    打开镜像关闭旧镜像

--- src/renderer/components/dashboard/Management.vue
+++ src/renderer/components/dashboard/Management.vue
@@ -120,6 +120,8 @@
 	import EditableCell from '../components/EditableCell'
 	import Regular from '@/utils/regular'
 	import { ipcRenderer } from 'electron'
+	const childProcess = require('child_process');
+	const exec = childProcess.exec
 	export default {
 		name: 'Devices',
 		data() {
@@ -231,10 +233,39 @@
 			installApp() {
 				ipcRenderer.send('installApp', {appLocation: this.appLocation, devices: this.activeDeviceIds})
 			},
+			viewProcessMessage(name, cb) {
+				let cmd = process.platform === 'win32' ? 'tasklist' : 'ps aux'
+				exec(cmd, function (err, stdout, stderr) {
+					if (err) {
+						return console.error(err)
+					}
+					stdout.split('\n').filter((line) => {
+						let processMessage = line.trim().split(/\s+/)
+						let processName = processMessage[0] //processMessage[0]进程名称 ， processMessage[1]进程id
+						if (processName === name) {
+							return cb(processMessage[1])
+						}
+					})
+				})
+			},
 			open() {
+				// for (let processId in this.activeDeviceIds) {
+				// 	console.log(this.activeDeviceIds[processId]);
+				// 	process.kill(processId, 'SIGTERM')
+				//
+				// }
+				// this.activeDeviceIds = {}
+
+				this.viewProcessMessage('scrcpy.exe',function (msg) {
+					//关闭匹配的进程
+					process.kill(msg)
+				})
+
 				this.$notify.info(this.$t('management.open.loading'), 2000)
 				const config = this.$store.get('config')
-				ipcRenderer.send('open', { config, devices: this.selectedDevices })
+				setTimeout(() => {
+					ipcRenderer.send('open', {config, devices: this.selectedDevices})
+				}, 1000)
 			},
 			connect() {
 				if (!Regular('ip', this.ip)) {
